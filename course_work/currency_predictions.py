4add8af8d1a4a68de5ccce3404040fe963c75d44
Yegor Kalyuzhnyy <yegorka.vegetablya@gmail.com>
2022-11-03 02:07:46 +0300

    Создание парсера для результатов запросов.

--- course_work/currency_predictions.py
+++ course_work/currency_predictions.py
@@ -1,4 +1,6 @@
 import requests
+import json
+from course_work.constants import JSON_RESULT_STRING
 
 
 def get_currency_information(days_number):
@@ -12,7 +14,19 @@ def get_currency_information(days_number):
     query += "source=RUB"
     query += "&"
     query += "apikey=ihujB93OoO8qFGK9mLXr1mdnNP8OxDtE"
+
     #response = requests.get(query)
-    #print(response.text)
+    #print("\n\n\n\n\n", response.text, "\n\n\n\n\n", sep="")
+
     # return response.text
     return "!"
+
+
+def parse_json_result_string(string=JSON_RESULT_STRING):
+    currency_information = json.loads(string)
+
+    result = dict()
+    for key in currency_information['quotes'].keys():
+        result[key] = currency_information['quotes'][key]['RUBDKK']
+
+    return result
